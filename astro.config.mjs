5bda60b80f5e703ec2316f64d8f02ec6d46f3051
Funasitien DreamCloud <hello@funa.dev>
2025-11-16 10:29:56 +0100

    ⚡️404 à notr avantage

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
+import lenis from "astro-lenis";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [icon()]
+  integrations: [icon(), mdx(), lenis()]
 });
